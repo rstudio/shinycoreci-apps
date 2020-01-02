c75b3e5961311b46db7f31fcc720b565ca607eb3
Winston Chang <winston@stdout.org>
2020-01-02 16:23:58 -0600

    Add space

--- 008-html/tests/mytest.R
+++ 008-html/tests/mytest.R
@@ -1,3 +1,3 @@
-app <- ShinyDriver$new("../", seed = 100,shinyOptions = list(display.mode = "normal"))
+app <- ShinyDriver$new("../", seed = 100, shinyOptions = list(display.mode = "normal"))
 app$snapshotInit("mytest")
 app$snapshot()
