888a040348740c59dd0e47adcfdd64c6e8ef47b2
Winston Chang <winston@stdout.org>
2020-01-08 15:13:57 -0600

    Longer delay for mathjax

--- 019-mathjax/tests/shinytests/mytest.R
+++ 019-mathjax/tests/shinytests/mytest.R
@@ -1,7 +1,7 @@
 app <- ShinyDriver$new("../../", seed = 100, shinyOptions = list(display.mode = "normal"))
 app$snapshotInit("mytest")
 
-Sys.sleep(2)
+Sys.sleep(3)
 app$snapshot()
 app$setInputs(ex5_visible = TRUE)
 Sys.sleep(2)
