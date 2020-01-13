814ca7651179a5f8926cb5a87afed26022918e06
Winston Chang <winston@stdout.org>
2020-01-13 15:21:49 -0600

    Update test results for mac

--- 012-datatables/tests/shinytests/mytest.R
+++ 012-datatables/tests/shinytests/mytest.R
@@ -11,6 +11,7 @@ app$setInputs(show_vars = c("carat", "cut", "color", "clarity", "depth", "price"
 app$setInputs(show_vars = c("carat", "cut", "color", "clarity", "price", "x", "y", "z"))
 # Input 'mytable1_rows_current' was set, but doesn't have an input binding.
 # Input 'mytable1_rows_all' was set, but doesn't have an input binding.
+Sys.sleep(0.5)
 app$snapshot()
 app$setInputs(show_vars = c("color", "clarity", "price", "y", "z"))
 # Input 'mytable1_rows_current' was set, but doesn't have an input binding.
@@ -25,6 +26,8 @@ app$setInputs(show_vars = c("y", "z"))
 # Input 'mytable1_rows_current' was set, but doesn't have an input binding.
 # Input 'mytable1_rows_all' was set, but doesn't have an input binding.
 app$setInputs(show_vars = "z")
+Sys.sleep(0.5)
 app$snapshot()
 app$setInputs(dataset = "mtcars")
+Sys.sleep(0.5)
 app$snapshot()
