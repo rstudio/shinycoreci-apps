d13c74dd8bc5d2b5c87e75cf290ebd5dd0629285
Winston Chang <winston@stdout.org>
2020-01-14 14:34:09 -0600

    Updates

--- 021-selectize-plot/tests/shinytests/mytest.R
+++ 021-selectize-plot/tests/shinytests/mytest.R
@@ -1,7 +1,7 @@
 app <- ShinyDriver$new("../../", seed = 100, shinyOptions = list(display.mode = "normal"))
 app$snapshotInit("mytest")
 
-Sys.sleep(2.5)
+Sys.sleep(4)
 app$snapshot()
 app$setInputs(state = "California")
 Sys.sleep(.5)
