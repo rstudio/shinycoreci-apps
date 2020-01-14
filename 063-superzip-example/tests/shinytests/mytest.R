0b6acd9509762d3756e306e6587caff8904cda1a
Winston Chang <winston@stdout.org>
2020-01-13 19:06:23 -0600

    Increase delay

--- 063-superzip-example/tests/shinytests/mytest.R
+++ 063-superzip-example/tests/shinytests/mytest.R
@@ -1,15 +1,15 @@
 app <- ShinyDriver$new("../../", seed = 100, shinyOptions = list(display.mode = "normal"))
 app$snapshotInit("mytest")
 
-Sys.sleep(2)
+Sys.sleep(4)
 app$snapshot()
 app$setInputs(threshold = 3)
 app$setInputs(color = "college")
-Sys.sleep(1)
+Sys.sleep(2)
 app$snapshot()
 
 app$setInputs(nav = "Data explorer")
-Sys.sleep(1)
+Sys.sleep(2)
 app$snapshot()
 # Input 'ziptable_rows_current' was set, but doesn't have an input binding.
 # Input 'ziptable_rows_all' was set, but doesn't have an input binding.
