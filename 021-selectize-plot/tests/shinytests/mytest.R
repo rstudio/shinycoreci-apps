d7359ec83550f5212e8c26b0eaf25fadf8bf6a9c
Winston Chang <winston@stdout.org>
2020-01-16 14:17:37 -0600

    Increase delay for 021

--- 021-selectize-plot/tests/shinytests/mytest.R
+++ 021-selectize-plot/tests/shinytests/mytest.R
@@ -1,7 +1,7 @@
 app <- ShinyDriver$new("../../", seed = 100, shinyOptions = list(display.mode = "normal"))
 app$snapshotInit("mytest")
 
-Sys.sleep(4)
+Sys.sleep(6)
 app$snapshot()
 app$setInputs(state = "California")
 Sys.sleep(1)
