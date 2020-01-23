39b93207b9114a58c14e3fb2a84e357e169795dd
Winston Chang <winston@stdout.org>
2020-01-23 10:22:40 -0600

    021: intelligently wait for value

--- 021-selectize-plot/tests/shinytests/mytest.R
+++ 021-selectize-plot/tests/shinytests/mytest.R
@@ -1,7 +1,15 @@
 app <- ShinyDriver$new("../../", seed = 100, shinyOptions = list(display.mode = "normal"))
 app$snapshotInit("mytest")
 
-Sys.sleep(6)
+# Spin until input$state gives us a useful value, or timeout.
+for (i in 1:100) {
+  state <- try(app$getValue("state", "input"))
+  if (inherits(state, "try-error") || identical(state, "")) {
+    Sys.sleep(0.2)
+  } else {
+    break
+  }
+}
 app$snapshot()
 app$setInputs(state = "California")
 Sys.sleep(1)
