4cbf936d99b8ee251e00c06f5be0a00064d1b11c
Winston Chang <winston@stdout.org>
2020-02-10 13:10:10 -0600

    Better wait for 134

--- apps/134-async-hold-timers/tests/shinytests/mytest.R
+++ apps/134-async-hold-timers/tests/shinytests/mytest.R
@@ -2,5 +2,10 @@ app <- ShinyDriver$new("../../")
 app$snapshotInit("mytest")
 
 app$snapshot()
-Sys.sleep(10)
+# This is the literal data structure that is sent for output$status. We're
+# waiting for it to change so that it no longer says "Waiting...". The data
+# structure was retrieved by using `dput(app$getAllValues()$output$status)`.
+app$waitForValue("status", iotype = "output", timeout = 20000,
+  ignore = list(list(html = structure("<h4>\n  <span style=\"background-color: #dddddd;\">Waiting...</span>\n</h4>", html = TRUE, class = c("html",
+"character")), deps = list())))
 app$snapshot()
