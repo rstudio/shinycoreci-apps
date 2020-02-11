6d2a29108eb2699120d68271889699dc1eb296ba
Winston Chang <winston@stdout.org>
2020-02-11 16:20:31 -0600

    Add wait for 065

--- apps/065-update-input-demo/tests/shinytests/mytest.R
+++ apps/065-update-input-demo/tests/shinytests/mytest.R
@@ -1,6 +1,7 @@
 app <- ShinyDriver$new("../../", seed = 100, shinyOptions = list(display.mode = "normal"))
 app$snapshotInit("mytest")
 
+app$waitForValue("inCheckboxGroup")
 app$snapshot()
 app$setInputs(control_num = 8)
 app$snapshot()
