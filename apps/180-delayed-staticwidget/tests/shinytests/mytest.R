aa23e02447a13b72bdb3df336d53ebcec8d20620
Barret Schloerke <barret@rstudio.com>
2020-02-18 17:18:50 -0500

    Always try to commit shinytest output (#12)
    * try to always push output from shinytest.  Separate out sysinfo commits. Use && to group commands.
    
    * gha- `sysinfo` Changes
    
    * gha- `shinytest` Changes
    
    * accept test updates to 179 and 180
    
    * gha- `sysinfo` Changes
    
    * gha- `shinytest` Changes
    
    * use the sha in the generated commit message
    
    * separate out shinytest commit and branch push. Make sure to only push if commits occured
    
    * fix workflow syntax
    
    * add commit diagnostics
    
    * compare against sha that caused action
    
    * ignore .Renviron file
    
    * fetch whole repo
    
    * do not track .Renviron
    
    * do not fetch whole repo
    
    * add wait before taking picture of map

--- apps/180-delayed-staticwidget/tests/shinytests/mytest.R
+++ apps/180-delayed-staticwidget/tests/shinytests/mytest.R
@@ -2,4 +2,6 @@ app <- ShinyDriver$new("../../", seed = 100, shinyOptions = list(display.mode = 
 app$snapshotInit("mytest")
 
 app$waitForValue("status", iotype = "output")
+
+Sys.sleep(2) # wait for map
 app$snapshot()
