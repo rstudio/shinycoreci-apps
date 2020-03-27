d713d083c0def99fc6202dc224e0887447ce4891
Barret Schloerke <schloerke@gmail.com>
2020-03-27 13:19:14 -0400

    wait for plot to appear before taking final snapshot

--- apps/062-submitbutton-demo/tests/shinytest/mytest.R
+++ apps/062-submitbutton-demo/tests/shinytest/mytest.R
@@ -1,12 +1,16 @@
 app <- ShinyDriver$new("../../", seed = 100, shinyOptions = list(display.mode = "normal"))
 app$snapshotInit("mytest")
 
+plotInit <- app$waitForValue("plot1", ignore = list(NULL, ""), iotype = "output")
 app$snapshot()
+
+# Set inputs
 app$setInputs(text = "hello world", wait_=FALSE, values_=FALSE)
 app$setInputs(n = 20, wait_=FALSE, values_=FALSE)
-
 # Make sure the app does NOT change yet.
 app$snapshot()
 
+# Hit submit button
 app$executeScript("$('button[type=\"submit\"]').trigger('click')")
+app$waitForValue("plot1", ignore = list(NULL, plotInit), iotype = "output")
 app$snapshot()
