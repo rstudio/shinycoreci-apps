3734e32641d8c1a262ed98cc34f563f1c6d465e0
schloerke <schloerke@users.noreply.github.com>
2020-04-01 17:06:37 +0000

    Generate Connect URLs (GitHub Actions)

---
+++ zzz_shinycoreci/connect/urls_betarstudioconnectcom_barret.R
@@ -0,0 +1,2 @@
+structure(c(`001-hello` = "https://beta.rstudioconnect.com/content/4892/", 
+`002-text` = "https://beta.rstudioconnect.com/content/4891/"), account = "barret", server = "beta.rstudioconnect.com")
