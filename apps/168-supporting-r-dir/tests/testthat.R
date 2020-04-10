f0dab7d13ccdac18a280aaef0a18cac4cf733e00
Alan Dipert <alan@dipert.org>
2020-04-10 22:49:08 +0000

    Update for new testServer() behavior

--- apps/168-supporting-r-dir/tests/testthat.R
+++ apps/168-supporting-r-dir/tests/testthat.R
@@ -5,4 +5,10 @@ library(shiny)
 # the files in R/, and the module we're testing was defined in those files. If
 # we don't specify env, testthat uses an environment that doesn't include the
 # module, and the tests fail.
-testthat::test_file("testthat/tests.R", env = environment())
+testthat::test_dir(
+  "testthat/",
+  env = environment(),
+  # Ensure that errors propagate out to runTests(), which is generally how app
+  # tests should be exercised.
+  stop_on_failure = TRUE
+)
