ebb467a201661a78e1da116ccf7ef07e17f94751
Barret Schloerke <schloerke@gmail.com>
2020-04-23 16:02:42 -0400

    Use flags to turn off some apps with shinyjster and shinytest
    022 - no shinytest on windows
    124 - no shinytest on mac
    124 - make a manual app
    156 - no shinyjster on IE

--- apps/022-unicode-chinese/tests/shinytest.R
+++ apps/022-unicode-chinese/tests/shinytest.R
@@ -1,2 +1,4 @@
+### Keep this line to NOT test this shiny application with shinycoreci::test_shinytest on Windows. Do not edit this line; shinycoreci::::not_shinytest_app_win
+
 library(shinytest)
 shinytest::testApp("../")
--- apps/124-async-download/tests/shinytest.R
+++ apps/124-async-download/tests/shinytest.R
@@ -1,3 +1,4 @@
+### Keep this line to NOT test this shiny application with shinycoreci::test_shinytest on Mac. Do not edit this line; shinycoreci::::not_shinytest_app_mac
+
 library(shinytest)
 shinytest::testApp("../")
-
