b915ad0c178aef511201496a6823218ffdcab9b7
Barret Schloerke <schloerke@gmail.com>
2020-04-28 14:47:05 -0400

    do not test 124 with shinytest. (It is a manual app.)

--- apps/063-superzip-example/tests/shinytest/mytest.R
+++ apps/063-superzip-example/tests/shinytest/mytest.R
@@ -1,31 +1,40 @@
 app <- ShinyDriver$new("../../", seed = 100, shinyOptions = list(display.mode = "normal"))
 
-print(1)
-print(app$getDebugLog())
-cat('\n\n')
+verbose <- FALSE
+if (verbose) {
+  print(1)
+  print(app$getDebugLog())
+  cat('\n\n')
+}
 app$snapshotInit("mytest")
 
 Sys.sleep(4)
 
-print(2)
-print(app$getDebugLog())
-cat('\n\n')
+if (verbose) {
+  print(2)
+  print(app$getDebugLog())
+  cat('\n\n')
+}
 app$snapshot()
 app$setInputs(threshold = 3)
 app$setInputs(color = "college")
 
 Sys.sleep(2)
-print(3)
-print(app$getDebugLog())
-cat('\n\n')
+if (verbose) {
+  print(3)
+  print(app$getDebugLog())
+  cat('\n\n')
+}
 app$snapshot()
 
 app$setInputs(nav = "Data explorer")
 
 Sys.sleep(2)
-print(4)
-print(app$getDebugLog())
-cat('\n\n')
+if (verbose) {
+  print(4)
+  print(app$getDebugLog())
+  cat('\n\n')
+}
 app$snapshot()
 
 # Input 'ziptable_rows_current' was set, but doesn't have an input binding.
@@ -35,7 +44,9 @@ app$setInputs(states = "MA")
 # Input 'ziptable_rows_all' was set, but doesn't have an input binding.
 
 Sys.sleep(1)
-print(5)
-print(app$getDebugLog())
-cat('\n\n')
+if (verbose) {
+  print(5)
+  print(app$getDebugLog())
+  cat('\n\n')
+}
 app$snapshot()
