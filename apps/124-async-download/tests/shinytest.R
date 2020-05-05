871ec76b7d77d1ab54888984ae250dcb7c7f677f
Barret Schloerke <schloerke@gmail.com>
2020-05-05 15:19:35 -0400

    Add a `if (FALSE)` for app 124 as it produces too many false positives

--- apps/124-async-download/tests/shinytest.R
+++ apps/124-async-download/tests/shinytest.R
@@ -1,4 +1,5 @@
-### Keep this line to NOT test this shiny application with shinycoreci::test_shinytest. Do not edit this line; shinycoreci::::not_shinytest_app
-
-library(shinytest)
-shinytest::testApp("../")
+# This application has a random issue about a url not being valid.  Better to manually test
+if (FALSE) {
+  library(shinytest)
+  shinytest::testApp("../")
+}
