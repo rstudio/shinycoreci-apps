3c43ba35313bb1ad17453475b1da9658418964ce
Carson Sievert <cpsievert1@gmail.com>
2020-05-12 11:14:19 -0500

    Use test_runtests (#51)

--- apps/022-unicode-chinese/tests/shinytest.R
+++ apps/022-unicode-chinese/tests/shinytest.R
@@ -1,4 +1,5 @@
-### Keep this line to NOT test this shiny application with shinycoreci::test_shinytest on Windows. Do not edit this line; shinycoreci::::not_shinytest_app_win
 
-library(shinytest)
-shinytest::testApp("../")
+# (default) windows has trouble with chinese characters. Better to test manually at a later time
+if (shinycoreci::platform() != "win") {
+  shinycoreci::test_shinytest_app()
+}
