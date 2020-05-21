58dbf50e2450eb3f33fc8eb294ff24121ccb2ae6
Carson <cpsievert1@gmail.com>
2020-05-21 10:19:44 -0500

    just go with type='cairo' (like we've always done)

--- apps/022-unicode-chinese/global.R
+++ apps/022-unicode-chinese/global.R
@@ -9,9 +9,9 @@ if (!file.exists('wqy-zenhei.ttc')) {
 if (.Platform[['OS.type']] == 'windows') {
   old_locale <- Sys.getlocale()
   Sys.setlocale(category = "LC_ALL", locale = "chs")
-  # showtext doesn't appear to support CairoPNG on Windows, so use ragg instead
-  systemfonts::register_font("WenQuanYI Zen Hei", "wqy-zenhei.ttc")
-  opts <- options(shiny.useragg = TRUE)
+  # showtext doesn't appear to support CairoPNG on Windows currently
+  # https://github.com/yixuan/showtext/issues/35
+  opts <- options(shiny.usecairo = FALSE)
   onStop(function() {
     cats <- strsplit(old_locale, ';')[[1]]
     lapply(cats, function(cat) {
@@ -20,11 +20,11 @@ if (.Platform[['OS.type']] == 'windows') {
     })
     options(opts)
   })
-} else {
-  sysfonts::font_add("WenQuanYI Zen Hei", "wqy-zenhei.ttc")
-  showtext::showtext_auto()
-  onStop(function() { showtext::showtext_auto(FALSE) })
 }
+
+sysfonts::font_add("WenQuanYI Zen Hei", "wqy-zenhei.ttc")
+showtext::showtext_auto()
+onStop(function() { showtext::showtext_auto(FALSE) })
 
 library(datasets)
 rock2 <- rock
