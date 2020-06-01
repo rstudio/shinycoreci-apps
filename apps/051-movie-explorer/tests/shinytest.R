89d41173c94894e246b7e9f0ad80b78e70339dd3
Carson <cpsievert1@gmail.com>
2020-06-01 13:52:00 -0500

    disable 051

--- apps/051-movie-explorer/tests/shinytest.R
+++ apps/051-movie-explorer/tests/shinytest.R
@@ -1,1 +1,6 @@
-shinycoreci::test_shinytest_app()
+# Doesn't want to consistently pass on all builds...there some sort of timing
+# issue where shinytest doesn't know how long to wait before taking a snapshot
+if (FALSE) {
+  shinycoreci::test_shinytest_app()
+}
+
