a38a396cda5eabeda57a297b192901b8141976cb
Barret Schloerke <schloerke@gmail.com>
2020-07-27 15:33:20 -0400

    wait for the value to arrive before taking a picture

--- apps/023-optgroup-server/tests/shinytest/mytest.R
+++ apps/023-optgroup-server/tests/shinytest/mytest.R
@@ -1,10 +1,13 @@
 app <- ShinyDriver$new("../../", seed = 100, shinyOptions = list(display.mode = "normal"))
 app$snapshotInit("mytest")
 
+row_init_value <- app$waitForValue("row", iotype = "output")
+
 app$snapshot()
 app$setInputs(group = "17")
-Sys.sleep(1)
+row1 <- app$waitForValue("row", ignore = list(row_init_value), iotype = "output")
 app$snapshot()
+
 app$setInputs(group = c("17", "30"))
 app$setInputs(group = c("17", "30", "22"))
 app$setInputs(group = c("17", "30", "22", "14"))
