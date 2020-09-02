bf3d10a4ce644906aa7a0398b863919fe95d769e
Barret Schloerke <schloerke@gmail.com>
2020-09-02 11:45:48 -0400

    update 194 shinytest test file

--- apps/194-plot-cache-key/tests/shinytest/mytest.R
+++ apps/194-plot-cache-key/tests/shinytest/mytest.R
@@ -1,12 +1,19 @@
 app <- ShinyDriver$new("../../", seed = 100, shinyOptions = list(display.mode = "normal"))
 app$snapshotInit("mytest")
 
-app$snapshot()
+take_snapshot <- function(refresh = TRUE) {
+  app$setInputs(`reactlog_module-refresh` = "click")
+  Sys.sleep(4)
+  app$snapshot()
+}
+take_snapshot(FALSE)
+
+
 app$setInputs(n = 250)
-app$snapshot()
+take_snapshot()
 app$setInputs(n = 100)
-app$snapshot()
+take_snapshot()
 app$setInputs(newdata = "click")
-app$snapshot()
+take_snapshot()
 app$setInputs(n = 300)
-app$snapshot()
+take_snapshot()
