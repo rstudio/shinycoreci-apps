f27645bd649a6bed388357ec6c488b2062ba8ee8
Barret Schloerke <schloerke@gmail.com>
2020-10-01 10:01:05 -0400

    Only test if highcharter >= 0.8.2 is installed

--- apps/201-flexdashboard-sales-report/tests/shinyjster-edge.R
+++ apps/201-flexdashboard-sales-report/tests/shinyjster-edge.R
@@ -1,1 +1,4 @@
-shinycoreci::test_shinyjster_app("edge")
+if (nzchar(system.file(package = "highcharter")) &&
+    packageVersion("highcharter") >= "0.8.2") {
+  shinycoreci::test_shinyjster_app("edge")
+}
--- apps/201-flexdashboard-sales-report/tests/shinytest.R
+++ apps/201-flexdashboard-sales-report/tests/shinytest.R
@@ -1,1 +1,4 @@
-shinycoreci::test_shinytest_app()
+if (nzchar(system.file(package = "highcharter")) &&
+    packageVersion("highcharter") >= "0.8.2") {
+  shinycoreci::test_shinytest_app()
+}
