3863a8780aaa50dd2bf15634c2924a1c08bef804
Carson <cpsievert1@gmail.com>
2020-12-07 10:39:45 -0600

    Use a more deployable approach

--- apps/195-input-theming-bs4-dark/app.R
+++ apps/195-input-theming-bs4-dark/app.R
@@ -20,12 +20,17 @@ theme <- bs_theme(
   code_font = font_google("Fira Code")
 )
 
+
+# If run interactively, inject bs_themer() into the demo app
+# so we can do manual tests with it
+app_dir <- system.file("themer-demo", package = "bslib")
 if (interactive()) {
-  bs_theme_preview(theme)
+  # Use this as opposed to bs_theme_preview() so we can deploy it
+  bslib:::as_themer_app(app_dir, theme)
 } else {
   old_theme <- bs_global_set(theme)
   onStop(function() {
     bs_global_set(old_theme)
   })
-  shinyAppDir(system.file("themer-demo", package = "bslib"))
+  shinyAppDir(app_dir)
 }
--- apps/198-input-theming-bs3-light/app.R
+++ apps/198-input-theming-bs3-light/app.R
@@ -7,12 +7,16 @@ library(DT)
 
 theme <- bs_theme(version = 3)
 
+# If run interactively, inject bs_themer() into the demo app
+# so we can do manual tests with it
+app_dir <- system.file("themer-demo", package = "bslib")
 if (interactive()) {
-  bs_theme_preview(theme)
+  # Use this as opposed to bs_theme_preview() so we can deploy it
+  bslib:::as_themer_app(app_dir, theme)
 } else {
   old_theme <- bs_global_set(theme)
   onStop(function() {
     bs_global_set(old_theme)
   })
-  shinyAppDir(system.file("themer-demo", package = "bslib"))
+  shinyAppDir(app_dir)
 }
