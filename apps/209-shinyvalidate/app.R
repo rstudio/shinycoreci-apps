96b7e41cd21f6260cff7d9c88f83d575127ce0c1
Nick Strayer <nick.strayer@gmail.com>
2020-12-11 12:45:11 -0500

    Date inputs and validations now dont depend on current date

--- apps/209-shinyvalidate/app.R
+++ apps/209-shinyvalidate/app.R
@@ -11,8 +11,8 @@ ui <- fluidPage(
   p("The shinytest starts by fulfilling all requirements and then rebreaking them."),
   textInput("text", ""),
   selectInput("select", "", multiple = TRUE, state.name),
-  dateInput("date", ""),
-  dateRangeInput("dateRange", ""),
+  dateInput("date", "", value = "2020-12-09"),
+  dateRangeInput("dateRange", "", start = "2020-12-09", end = "2020-12-11"),
   sliderInput("slider", "", min = 0, max = 100, value = 50, step = 1),
   checkboxInput("checkbox", "", value = FALSE),
   checkboxGroupInput("checkboxGroup", "", choices = c("A", "B")),
@@ -23,8 +23,8 @@ server <- function(input, output, session) {
   iv <- InputValidator$new()
   iv$add_rule("text", sv_required("Enter some text"))
   iv$add_rule("select", sv_required("Choose a state"))
-  iv$add_rule("date", ~if (. <= Sys.Date()) "Choose a future date!")
-  iv$add_rule("dateRange", ~if (min(.) <= Sys.Date()) "Choose future dates!")
+  iv$add_rule("date", ~if (. <= as.Date("2020-12-10")) "Choose a date after Dec 10th, 2020!")
+  iv$add_rule("dateRange", ~if (min(.) <= as.Date("2020-12-10")) "Choose range after Dec 10th, 2020!")
   iv$add_rule("slider", ~if (. %% 2 == 0) "Choose an odd number!")
   iv$add_rule("checkbox", ~if (!.) "Please check true!")
   iv$add_rule("checkboxGroup", ~if (!identical(., "B")) "Please check (just) B!")
