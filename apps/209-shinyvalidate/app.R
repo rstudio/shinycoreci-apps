c1d23dba4bb3c9d50b1ebe1512de3946b8b5d291
Nick Strayer <nick.strayer@gmail.com>
2020-12-11 12:44:20 -0500

    Added theme toggling radio buttons

--- apps/209-shinyvalidate/app.R
+++ apps/209-shinyvalidate/app.R
@@ -2,8 +2,11 @@ library(shiny)
 library(shinyvalidate)
 library(bslib)
 
+
+my_theme <- bs_theme(bootswatch = "darkly")
 ui <- fluidPage(
-  theme = bs_theme(danger = "orange"),
+  theme = my_theme,
+  radioButtons("current_theme", "Choose a theme for bslib", choices = c("darkly", "flatly", "minty")),
   p("This app was taken from",  a("this PR", href = "https://github.com/rstudio/shinyvalidate/pull/4"), "on the shinyvalidate repo."),
   p("The shinytest starts by fulfilling all requirements and then rebreaking them."),
   textInput("text", ""),
@@ -27,6 +30,24 @@ server <- function(input, output, session) {
   iv$add_rule("checkboxGroup", ~if (!identical(., "B")) "Please check (just) B!")
   iv$add_rule("radio", ~if (!identical(., "B")) "Please check B!")
   iv$enable()
+
+  # Taken from
+  # https://github.com/rstudio/bslib/blob/master/R/bs-theme-preview.R#L300-L302
+  # To setup the proper sass variables to inject for theme changing related to
+  # bootswatch change
+  # We reimplement this here so we can expose the input to shinytest for testing
+  themer_opts <- bslib:::opts_metadata()
+  themer_vars <- unlist(unname(lapply(themer_opts, names)))
+  sass_vars <- setdiff(themer_vars, "bootswatch")
+
+  observe({
+    # Watch for the change request here
+    my_theme <<- bslib:::set_current_theme(
+      my_theme, list(bootswatch = input$current_theme), session
+    )
+    vals <- as.list(bs_get_variables(my_theme, sass_vars))
+    session$sendCustomMessage("bs-themer-bootswatch", list(values = vals))
+  })
 }
 
 shinyApp(ui, server)
