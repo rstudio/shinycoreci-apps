e43cbf94f54082d19faaa0e50aada37fd564b1f0
Carson <cpsievert1@gmail.com>
2021-01-05 19:44:18 -0600

    Forgot to modify 187-navbar-collapse to use BS4

--- apps/187-navbar-collapse/app.R
+++ apps/187-navbar-collapse/app.R
@@ -44,8 +44,7 @@ jster <- shinyjster::shinyjster_js(
     "
 )
 
-ui <- navbarPage(
-  theme = bslib::bs_global_get(),
+ui <- navbarPage(theme = bslib::bs_theme(),
   "", collapsible = TRUE,
   tabPanel("Plot", msg, plotOutput("plot")),
   tabPanel("Summary", msg, verbatimTextOutput("summary"), jster)
