347abfc0298ceb58d8a97f4a610692acfa284449
Carson <cpsievert1@gmail.com>
2021-01-05 19:10:16 -0600

    117-shinythemes needs to be using sidebarLayout()

--- apps/117-shinythemes/app.R
+++ apps/117-shinythemes/app.R
@@ -7,32 +7,35 @@ shinyApp(
     navbarPage(
       theme = bslib::bs_theme(),
       "shinythemes",
-      tabPanel("Navbar 1",
-        sidebarPanel(
-          fileInput("file", "File input:"),
-          textInput("txt", "Text input:", "general"),
-          sliderInput("slider", "Slider input:", 1, 100, 30),
-          tags$h5("Deafult actionButton:"),
-          actionButton("action", "Search"),
+      tabPanel(
+        "Navbar 1",
+        sidebarLayout(
+          sidebarPanel(
+            fileInput("file", "File input:"),
+            textInput("txt", "Text input:", "general"),
+            sliderInput("slider", "Slider input:", 1, 100, 30),
+            tags$h5("Deafult actionButton:"),
+            actionButton("action", "Search"),
 
-          tags$h5("actionButton with CSS class:"),
-          actionButton("action2", "Action button", class = "btn-primary")
-        ),
-        mainPanel(
-          tabsetPanel(
-            tabPanel("Tab 1",
-              h4("Table"),
-              tableOutput("table"),
-              h4("Verbatim text output"),
-              verbatimTextOutput("txtout"),
-              h1("Header 1"),
-              h2("Header 2"),
-              h3("Header 3"),
-              h4("Header 4"),
-              h5("Header 5")
-            ),
-            tabPanel("Tab 2", "This panel is intentionally left blank"),
-            tabPanel("Tab 3", "This panel is intentionally left blank")
+            tags$h5("actionButton with CSS class:"),
+            actionButton("action2", "Action button", class = "btn-primary")
+          ),
+          mainPanel(
+            tabsetPanel(
+              tabPanel("Tab 1",
+                       h4("Table"),
+                       tableOutput("table"),
+                       h4("Verbatim text output"),
+                       verbatimTextOutput("txtout"),
+                       h1("Header 1"),
+                       h2("Header 2"),
+                       h3("Header 3"),
+                       h4("Header 4"),
+                       h5("Header 5")
+              ),
+              tabPanel("Tab 2", "This panel is intentionally left blank"),
+              tabPanel("Tab 3", "This panel is intentionally left blank")
+            )
           )
         )
       ),
