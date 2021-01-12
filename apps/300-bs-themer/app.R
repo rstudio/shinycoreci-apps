1e5fdb0437ac8c24167dfcd7700fa118cb285463
Barret Schloerke <schloerke@gmail.com>
2021-01-12 18:11:37 -0500

    add all deps to 300-bs-themer

--- apps/300-bs-themer/app.R
+++ apps/300-bs-themer/app.R
@@ -1,10 +1,35 @@
 ### Keep this line to manually test this shiny application. Do not edit this line; shinycoreci::::is_manual_app
-library(shiny)
+
+
 library(bslib)
-library(ggplot2)
-library(thematic)
-library(sf)
-library(DT)
+
+# used to find app deps
+if (FALSE) {
+
+  # system.file("themer-demo", package = "bslib") %>%
+  #   renv::dependencies(
+  #     ,
+  #     quiet = TRUE
+  #   ) %>%
+  #   {.$Package} %>%
+  #   unique() %>%
+  #   sort() %>%
+  #   paste0("library(", ., ")", collapse = "\n") %>%
+  #   cat()
+
+  library(curl)
+  library(DT)
+  library(ggplot2)
+  library(hexbin)
+  library(lattice)
+  library(reactable)
+  library(rlang)
+  library(rprojroot)
+  library(rsconnect)
+  library(shiny)
+  library(thematic)
+  library(tools)
+}
 
 # Essentially the same as bs_theme_preview(), but deployable
 old_theme <- bs_global_set(bs_theme())
