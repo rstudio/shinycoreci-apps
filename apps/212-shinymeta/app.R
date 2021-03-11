9e3dc778e6bc2cd32c9f576a7c7f4be92b43532c
Joe Cheng <joe@rstudio.com>
2021-03-11 12:01:08 -0800

    wip

---
+++ apps/212-shinymeta/app.R
@@ -0,0 +1,78 @@
+library(shiny)
+library(shinymeta)
+library(magrittr)
+
+selectColumnUI <- function(id, label) {
+  ns <- NS(id)
+  tagList(
+    varSelectInput(ns("col"), label, NULL),
+    textOutput(ns("average"))
+  )
+}
+
+selectColumn <- function(input, output, session, df) {
+  observeEvent(df(), {
+    updateVarSelectInput(session, "col", data = df())
+  })
+
+  values <- metaReactive2({
+    req(input$col)
+    metaExpr({
+      ..(df()) %>%
+        dplyr::pull(!!..(input$col))
+    })
+  })
+
+  avg <- metaReactive({
+    ..(values()) %>%
+      mean() %>%
+      round(1)
+  })
+
+  output$average <- metaRender(renderText, {
+    paste("Average of", ..(as.character(input$col)), "is", ..(avg()))
+  })
+
+  list(
+    values = values,
+    average = output$average
+  )
+}
+
+ui <- fluidPage(
+  fluidRow(
+    column(3, selectColumnUI("x", "x var")),
+    column(3, selectColumnUI("y", "y var"))
+  ),
+  outputCodeButton(plotOutput("plot")),
+  shinyjster::shinyjster_js(paste(collapse = "\n", readLines("shinyjster.js")))
+)
+
+server <- function(input, output, session) {
+  # include shinyjster_server call at top of server definition
+  shinyjster::shinyjster_server(input, output)
+
+  dataset <- metaReactive({mtcars})
+
+  x <- callModule(selectColumn, "x", dataset)
+  y <- callModule(selectColumn, "y", dataset)
+
+  df_plot <- metaReactive({
+    "# Combine x and y into data frame for plotting"
+    data.frame(x = ..(x$values()), y = ..(y$values()))
+  })
+
+  output$plot <- metaRender(renderPlot, {
+    plot(..(df_plot()))
+  })
+
+  observeEvent(input$plot_output_code, {
+    displayCodeModal(expandChain(
+      output$plot(),
+      x$average(),
+      y$average()
+    ))
+  })
+}
+
+shinyApp(ui, server)
---
+++ apps/212-shinymeta/shinyjster.js
@@ -0,0 +1,9 @@
+var jst = jster();
+jst.add(Jster.shiny.waitUntilStable);
+
+// TODO: select xvar/yvar
+// TODO: click button
+// TODO: wait for modal to appear
+// TODO: check $(".shiny-ace").data("aceEditor").session.doc.getAllLines()
+
+jst.test();
