7f95ca3b413f7762218134bc953d468228bcc882
Madhulika Tanuboddi <madhulika@rstudio.com>
2021-05-05 12:50:59 -0700

    Update "waitForValue" input to use plotly ID This make sures shinytest is waiting long enough for the first snapshot's valueBox to be updated

--- apps/200-flexdashboard-render-text/tests/shinytest/mytest.R
+++ apps/200-flexdashboard-render-text/tests/shinytest/mytest.R
@@ -1,7 +1,7 @@
 app <- ShinyDriver$new("../../index.Rmd", seed = 75237)
 app$snapshotInit("mytest")
 
-app$waitForValue("month")
+app$waitForValue("plotly_afterplot-A")
 app$snapshot()
 
 app$setInputs(month = "Mar")
