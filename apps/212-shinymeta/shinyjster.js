c951b0742ed3cc82bd6e386fdeb0148e3ab82a0e
Joe Cheng <joe@rstudio.com>
2021-05-06 12:04:10 -0700

    wip

--- apps/212-shinymeta/shinyjster.js
+++ apps/212-shinymeta/shinyjster.js
@@ -1,9 +1,30 @@
+function selectOption(jst, id, index) {
+  jst.add(function() {
+    Jster.selectize.click(id);
+  });
+  jst.add(Jster.shiny.waitUntilStable);
+  jst.add(function() {
+    Jster.selectize.clickOption(id, index);
+  })
+}
+
 var jst = jster();
 jst.add(Jster.shiny.waitUntilStable);
 
-// TODO: select xvar/yvar
-// TODO: click button
-// TODO: wait for modal to appear
-// TODO: check $(".shiny-ace").data("aceEditor").session.doc.getAllLines()
+selectOption(jst, "x-col", 1);
+selectOption(jst, "y-col", 2);
+jst.add(Jster.shiny.waitUntilStable);
+jst.add(function() {
+  var btn = $("button");
+  if (btn.length !== 1) {
+    throw new Error("One and only one button was expected, found " + btn.length);
+  }
+  btn.click();
+});
+jst.add(Jster.shiny.waitUntilStable);
+jst.add(function() {
+  var lines = $(".shiny-ace").data("aceEditor").session.doc.getAllLines();
+  // TODO: Check that line 2(?) is `dplyr::pull(!!as.symbol("cyl"))`, etc.
+});
 
 jst.test();
