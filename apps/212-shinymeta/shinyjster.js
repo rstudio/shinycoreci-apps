df769da92bc030b1cfc4d136364b128d0bf5b362
Carson <cpsievert1@gmail.com>
2021-05-06 14:32:47 -0500

    Add an expectation for the source code

--- apps/212-shinymeta/shinyjster.js
+++ apps/212-shinymeta/shinyjster.js
@@ -5,7 +5,7 @@ function selectOption(jst, id, index) {
   jst.add(Jster.shiny.waitUntilStable);
   jst.add(function() {
     Jster.selectize.clickOption(id, index);
-  })
+  });
 }
 
 var jst = jster();
@@ -24,7 +24,10 @@ jst.add(function() {
 jst.add(Jster.shiny.waitUntilStable);
 jst.add(function() {
   var lines = $(".shiny-ace").data("aceEditor").session.doc.getAllLines();
-  // TODO: Check that line 2(?) is `dplyr::pull(!!as.symbol("cyl"))`, etc.
+  Jster.assert.isEqual(
+    lines.join(" \n "),
+    "dataset <- mtcars \n x_values <- dataset %>% \n   dplyr::pull(!!as.symbol(\"cyl\")) \n y_values <- dataset %>% \n   dplyr::pull(!!as.symbol(\"disp\")) \n # Combine x and y into data frame for plotting \n df_plot <- data.frame(x = x_values, y = y_values) \n plot(df_plot) \n x_avg <- x_values %>% \n   mean() %>% \n   round(1) \n paste(\"Average of\", \"cyl\", \"is\", x_avg) \n y_avg <- y_values %>% \n   mean() %>% \n   round(1) \n paste(\"Average of\", \"disp\", \"is\", y_avg)"
+  );
 });
 
 jst.test();
