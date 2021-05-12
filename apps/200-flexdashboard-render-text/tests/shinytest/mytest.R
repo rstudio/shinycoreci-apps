ae4243437bd0c0943d92917a806f4145fc5beebf
MadhulikaTanuboddi <madhulika@rstudio.com>
2021-05-12 15:40:22 -0700

    "200-flexdashboard-render-text" shinytest fixes (#141)
    Co-authored-by: Madhulika Tanuboddi <madhulika@rstudio.com>
    Co-authored-by: Barret Schloerke <schloerke@gmail.com>

--- apps/200-flexdashboard-render-text/tests/shinytest/mytest.R
+++ apps/200-flexdashboard-render-text/tests/shinytest/mytest.R
@@ -7,6 +7,7 @@ app$snapshot()
 app$setInputs(month = "Mar")
 app$snapshot()
 
-# view second page
+# View second page
 app$executeScript('$("#navbar li a").last().click();')
+app$waitForValue("p2r1content", iotype = "output")
 app$snapshot()
