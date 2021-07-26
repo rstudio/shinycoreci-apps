71917cd03ae13737fb173b19902cf498a6763ec4
MadhulikaTanuboddi <mdhlk88@gmail.com>
2021-07-26 00:01:52 -0700

    Update modal-dialog app and shinytest with selectizeInput

--- apps/114-modal-dialog/tests/shinytest/mytest.R
+++ apps/114-modal-dialog/tests/shinytest/mytest.R
@@ -5,6 +5,14 @@ app$setInputs(show = "click")
 Sys.sleep(1)
 app$snapshot()
 
+# Click selectize input - https://github.com/rstudio/shiny/pull/3450
+app$executeScript("$('.selectize-input').click()");
+app$snapshot()
+
+# Select an option
+app$setInputs(selectizeInput = "California")
+app$snapshot()
+
 # Click the Dismiss button
 app$executeScript("$('button[data-dismiss=\"modal\"]').click()")
 Sys.sleep(1)
