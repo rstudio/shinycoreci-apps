4a2e7854e588e04846a8aca08ecd635616634095
MadhulikaTanuboddi <mdhlk88@gmail.com>
2021-07-28 16:44:42 -0700

    Update modal-dialog app and shinytest with selectizeInput (#166)
    * Update modal-dialog app and shinytest with selectizeInput
    
    * Updated Readme

--- apps/114-modal-dialog/app.R
+++ apps/114-modal-dialog/app.R
@@ -7,6 +7,8 @@ shinyApp(
       showModal(modalDialog(
         title = "Important message",
         "This is an important message!",
+        hr(),
+        selectInput('selectizeInput', 'Selectize options', state.name, selectize=TRUE),
         easyClose = TRUE
       ))
     })
