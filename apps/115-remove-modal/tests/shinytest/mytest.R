29ffdddd71f19b4e9588f5f745d250580c37cf40
MadhulikaTanuboddi <mdhlk88@gmail.com>
2021-07-29 15:39:57 -0700

    Updated shinytest script based on PR feedback and discussion

--- apps/115-remove-modal/tests/shinytest/mytest.R
+++ apps/115-remove-modal/tests/shinytest/mytest.R
@@ -1,11 +1,21 @@
 app <- ShinyDriver$new("../../", seed = 100, shinyOptions = list(display.mode = "normal"))
 app$snapshotInit("mytest")
 
+# Verify modal is open
+app$executeScript(
+  "window.modalShown = false;
+  $(document).on('shown.bs.modal', function(e) { window.modalShown = true; });"
+)
 app$setInputs(openModalBtn = "click")
-Sys.sleep(1)
+app$waitFor("window.modalShown")
 app$snapshot()
 
+
 # Verify modal is closed
+app$executeScript(
+  "window.modalHidden = false;
+  $(document).on('hidden.bs.modal', function(e) {window.modalHidden = true; });"
+)
 app$setInputs(closeModalBtn = "click")
-Sys.sleep(1)
+app$waitFor("window.modalHidden")
 app$snapshot()
