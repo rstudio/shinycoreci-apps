b8b06771a482faaeab4f06debeb581968f6124aa
Barret Schloerke <schloerke@gmail.com>
2021-08-05 10:28:26 -0400

    Remove DESCRIPTION file

--- apps/216-quosures/tests/shinytest/mytest.R
+++ apps/216-quosures/tests/shinytest/mytest.R
@@ -1,7 +1,5 @@
 app <- ShinyDriver$new("../../", seed = 100, shinyOptions = list(display.mode = "normal"))
 app$snapshotInit("mytest")
 
-# Verify a diagram appears. Ref: https://github.com/rstudio/shiny/pull/3472.
-# For more details, check README of this app
 app$snapshot()
 
