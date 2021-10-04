e2ba2de36315c38196fb07f9fc6e13e30d2395b7
Barret Schloerke <schloerke@gmail.com>
2021-10-04 13:16:20 -0400

    debug tests

--- apps/217-snapshot-info-description/tests/testthat/tests.R
+++ apps/217-snapshot-info-description/tests/testthat/tests.R
@@ -1,10 +1,20 @@
 test_that("text passes", {
   app <- shinytest::ShinyDriver$new("../../", seed = 100, shinyOptions = list(display.mode = "normal"))
 
-  app$waitForValue("status", iotype = "output", ignore = list(NULL, ""))
+  val <- app$waitForValue("status", iotype = "output", ignore = list(NULL, ""))
+  cat("val:\n")
+  str(val)
+
+  for (i in 1:50) {
+    Sys.sleep(0.5)
+    textVal <- app$findElement("#status")$getText()
+    if (textVal != "") break
+  }
+  cat("textVal:\n")
+  str(textVal)
 
   testthat::expect_equal(
-    app$findElement("#status")$getText(),
+    textVal,
     "PASS"
   )
 })
