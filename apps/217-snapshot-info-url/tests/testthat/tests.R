ac6b48bc7077d5433e601ff332d99850d288b269
Barret Schloerke <schloerke@gmail.com>
2021-10-04 12:44:37 -0400

    ignore more things, hopefully it helps

--- apps/217-snapshot-info-url/tests/testthat/tests.R
+++ apps/217-snapshot-info-url/tests/testthat/tests.R
@@ -1,7 +1,7 @@
 test_that("text passes", {
   app <- shinytest::ShinyDriver$new("../../", seed = 100, shinyOptions = list(display.mode = "normal"))
 
-  app$waitForValue("status", iotype = "output")
+  app$waitForValue("status", iotype = "output", ignore = list(NULL, ""))
 
   testthat::expect_equal(
     app$findElement("#status")$getText(),
