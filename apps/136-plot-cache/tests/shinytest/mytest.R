79eeabf56cb90e7fe5f63707334e9d127cac06c8
Barret Schloerke <schloerke@gmail.com>
2022-04-01 11:31:53 -0400

    136-plot-cache

--- apps/136-plot-cache/tests/shinytest/mytest.R
+++
@@ -1,8 +0,0 @@
-app <- ShinyDriver$new("../../", seed = 100, shinyOptions = list(display.mode = "normal"))
-app$snapshotInit("mytest")
-
-app$snapshot()
-app$setInputs(n = 24)
-app$snapshot()
-app$setInputs(n = 8)
-app$snapshot()
