a88cc12cb730a45fccd8230133edac3e66a41640
Barret Schloerke <schloerke@gmail.com>
2022-04-01 12:06:21 -0400

    216-quosures

---
+++ apps/216-quosures/tests/testthat/test-mytest.R
@@ -0,0 +1,14 @@
+library(shinytest2)
+
+test_that("Migrated shinytest test: mytest.R", {
+  app <- AppDriver$new(variant = shinycoreci::platform_rversion(),
+    seed = 100, shiny_args = list(display.mode = "normal"))
+
+  app$expect_values()
+  app$expect_screenshot()
+
+  app$set_inputs(n = "click")
+
+  app$expect_values()
+  app$expect_screenshot()
+})
