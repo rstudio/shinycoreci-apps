6f1a3ce2f65db3d05394a4eb66b5a2cd0581c1f7
Barret Schloerke <schloerke@gmail.com>
2022-04-01 11:47:13 -0400

    180-delayed-staticwidget

--- apps/180-delayed-staticwidget/tests/shinytest/mytest.R
+++
@@ -1,7 +0,0 @@
-app <- ShinyDriver$new("../../", seed = 100, shinyOptions = list(display.mode = "normal"))
-app$snapshotInit("mytest")
-
-app$waitForValue("status", iotype = "output")
-
-Sys.sleep(2) # wait for map
-app$snapshot()
