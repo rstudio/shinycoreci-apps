c95348f057b4a529593804f25ea9f203a47dbef3
Barret Schloerke <schloerke@gmail.com>
2022-04-01 10:22:24 -0400

    001-hello

--- apps/001-hello/tests/shinytest/mytest.R
+++
@@ -1,9 +0,0 @@
-app <- ShinyDriver$new("../../", seed = 100, shinyOptions = list(display.mode = "normal"))
-app$snapshotInit("mytest")
-
-app$snapshot()
-app$setInputs(bins = 8)
-app$snapshot()
-app$setInputs(bins = 5)
-app$setInputs(bins = 22)
-app$snapshot()
