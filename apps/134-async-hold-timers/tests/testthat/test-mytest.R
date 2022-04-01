29c9b72582132eb7006c10cb26d07ccf50ec10ca
Barret Schloerke <schloerke@gmail.com>
2022-04-01 12:43:13 -0400

    Adjust 134 logic. Run tests

--- apps/134-async-hold-timers/tests/testthat/test-mytest.R
+++ apps/134-async-hold-timers/tests/testthat/test-mytest.R
@@ -8,7 +8,7 @@ test_that("Migrated shinytest test: mytest.R", {
   # This is the literal data structure that is sent for output$status. We're
   # waiting for it to change so that it no longer says "Waiting...". The data
   # structure was retrieved by using `dput(app$getAllValues()$output$status)`.
-  app$wait_for_value(output = "status", ignore = list(list(html = structure("<h4>\n  <span style=\"background-color: #dddddd;\">Waiting...</span>\n</h4>",
+  app$wait_for_value(output = "status", ignore = list(NULL, list(html = structure("<h4>\n  <span style=\"background-color: #dddddd;\">Waiting...</span>\n</h4>",
     html = TRUE, class = c("html", "character")), deps = list())),
     timeout = 20000)
   app$expect_values()
