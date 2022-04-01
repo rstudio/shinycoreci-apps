b6d38da0bbb139df6db48b6c9fa6fae259727c0d
Barret Schloerke <schloerke@gmail.com>
2022-04-01 10:57:17 -0400

    024-optgroup-selectize

---
+++ apps/024-optgroup-selectize/tests/testthat/test-mytest.R
@@ -0,0 +1,17 @@
+library(shinytest2)
+
+test_that("Migrated shinytest test: mytest.R", {
+  app <- AppDriver$new(variant = shinycoreci::platform_rversion(),
+    seed = 100, shiny_args = list(display.mode = "normal"))
+
+  app$wait_for_value(input = "x2")
+  app$expect_values()
+  app$expect_screenshot()
+  app$set_inputs(x1 = "CA")
+  app$set_inputs(x4 = "NJ")
+  app$expect_values()
+  app$expect_screenshot()
+  app$set_inputs(x2 = "OR")
+  app$expect_values()
+  app$expect_screenshot()
+})
