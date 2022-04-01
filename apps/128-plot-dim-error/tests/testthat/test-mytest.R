da5b0656c8e5fe1fc6c2d437188e59984ebfb292
Barret Schloerke <schloerke@gmail.com>
2022-04-01 11:26:45 -0400

    128-plot-dim-error

---
+++ apps/128-plot-dim-error/tests/testthat/test-mytest.R
@@ -0,0 +1,11 @@
+library(shinytest2)
+
+test_that("Migrated shinytest test: mytest.R", {
+  app <- AppDriver$new(variant = shinycoreci::platform_rversion())
+
+  app$expect_values()
+  app$expect_screenshot()
+  app$set_inputs(go = "click")
+  app$expect_values()
+  app$expect_screenshot()
+})
