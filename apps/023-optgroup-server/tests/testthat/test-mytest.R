4aac8ec2f5150923737280ad9ad284a711e15d07
Barret Schloerke <schloerke@gmail.com>
2022-04-01 12:12:05 -0400

    Fix logic assumptions

--- apps/023-optgroup-server/tests/testthat/test-mytest.R
+++ apps/023-optgroup-server/tests/testthat/test-mytest.R
@@ -4,12 +4,13 @@ test_that("Migrated shinytest test: mytest.R", {
   app <- AppDriver$new(variant = shinycoreci::platform_rversion(),
     seed = 100, shiny_args = list(display.mode = "normal"))
 
-  row_init_value <- app$wait_for_value(output = "row")
-
+  app$wait_for_idle()
   app$expect_values()
   app$expect_screenshot()
+
   app$set_inputs(group = "17")
-  row1 <- app$wait_for_value(output = "row", ignore = list(row_init_value))
+
+  app$wait_for_idle()
   app$expect_values()
   app$expect_screenshot()
 
@@ -19,7 +20,8 @@ test_that("Migrated shinytest test: mytest.R", {
   app$set_inputs(group = c("17", "30", "22", "14", "20"))
   app$set_inputs(group = c("17", "30", "22", "14", "20", "8"))
   app$set_inputs(group = c("17", "30", "22", "14", "20", "8", "12"))
-  Sys.sleep(1)
+
+  app$wait_for_idle()
   app$expect_values()
   app$expect_screenshot()
 })
--- apps/024-optgroup-selectize/tests/testthat/test-mytest.R
+++ apps/024-optgroup-selectize/tests/testthat/test-mytest.R
@@ -4,7 +4,6 @@ test_that("Migrated shinytest test: mytest.R", {
   app <- AppDriver$new(variant = shinycoreci::platform_rversion(),
     seed = 100, shiny_args = list(display.mode = "normal"))
 
-  app$wait_for_value(input = "x2")
   app$expect_values()
   app$expect_screenshot()
   app$set_inputs(x1 = "CA")
