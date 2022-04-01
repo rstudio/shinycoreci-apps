a1d98643a43f5af4a8ed31c73daffa84895bb607
Barret Schloerke <schloerke@gmail.com>
2022-04-01 10:34:48 -0400

    019-mathjax

--- apps/019-mathjax/tests/shinytest/mytest.R
+++
@@ -1,8 +0,0 @@
-app <- ShinyDriver$new("../../", seed = 100, loadTimeout = 15000, shinyOptions = list(display.mode = "normal"))
-app$snapshotInit("mytest")
-
-Sys.sleep(3)
-app$snapshot()
-app$setInputs(ex5_visible = TRUE)
-Sys.sleep(2)
-app$snapshot()
