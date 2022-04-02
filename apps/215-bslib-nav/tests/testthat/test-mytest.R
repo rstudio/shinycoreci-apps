7ca183beef8ab268fedf4c248ed989651586b4f2
Barret Schloerke <schloerke@gmail.com>
2022-04-01 22:45:24 -0400

    Fix tests for 215

--- apps/215-bslib-nav/tests/testthat/test-mytest.R
+++ apps/215-bslib-nav/tests/testthat/test-mytest.R
@@ -3,13 +3,19 @@ library(shinytest2)
 test_that("Migrated shinytest test: mytest.R", {
   app <- AppDriver$new(variant = shinycoreci::platform_rversion())
 
-  app$run_js(script = "$(\".nav [data-value=b]\").tab(\"show\")")
-  app$wait_for_js("{let b = $(\".tab-pane[data-value=b]\"); let bv = b.find(\":visible\"); b.length == bv.length > 0}")
-  app$expect_values()
-  app$expect_screenshot()
+  expect_tab <- function(name) {
+    app$run_js(script = paste0("$(\".nav [data-value=", name, "]\").tab(\"show\")"))
+    # Wait for at least one tab to be shown
+    app$wait_for_js(paste0('
+      $(".tab-pane[data-value=', name, ']").
+        map(function(){ return $(this).is(":visible") }).
+        toArray().
+        some((x) => x)
+    '))
+    app$expect_values()
+    app$expect_screenshot()
+  }
 
-  app$run_js(script = "$(\".nav [data-value=c]\").tab(\"show\")")
-  app$wait_for_js("var c = $(\".tab-pane[data-value=c]\"); var cv = c.find(\":visible\"); c.length == cv.length > 0")
-  app$expect_values()
-  app$expect_screenshot()
+  expect_tab("b")
+  expect_tab("c")
 })
