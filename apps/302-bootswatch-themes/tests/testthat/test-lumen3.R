60134daa55e93663ce356157a4bce59d9d995b52
Barret Schloerke <schloerke@gmail.com>
2022-04-04 10:12:42 -0400

    Convert files for 302

---
+++ apps/302-bootswatch-themes/tests/testthat/test-lumen3.R
@@ -0,0 +1,25 @@
+library(shinytest2)
+
+test_that("Migrated shinytest test: lumen3.R", {
+  # Do not edit this test script by hand. This script was generated automatically by 
+  # ./app/shinytest-template.R & ./app/tests/shinytest.R
+  # --------------------------------------------------------------------------------
+  # Do not edit this test script by hand. This script was generated automatically by
+  # ./app/shinytest-template.R & ./app/tests/shinytest.R
+  # --------------------------------------------------------------------------------
+
+  library(shinytest)
+  library(bslib)
+  theme <- yaml::yaml.load_file('../../themes.yaml', eval.expr = TRUE)[['lumen3']]
+  if (!is_bs_theme(theme)) {
+    theme <- do.call(bs_theme, theme)
+  }
+
+  # This `shinytest::` below is a hack to avoid shiny::runTests() thinking this
+  # template is itself a testing app https://github.com/rstudio/shiny/blob/24a1ef/R/test.R#L36
+  app <- AppDriver$new(variant = shinycoreci::platform_rversion(),
+    seed = 101, options = list(bslib_theme = theme))
+
+  app$expect_values()
+  app$expect_screenshot()
+})
